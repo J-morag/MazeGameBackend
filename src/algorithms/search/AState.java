d15db55a30a92e8fac43831b9491f06618b752e7
J-morag <38430800+J-morag@users.noreply.github.com>
2018-04-23 18:00:33 +0300

    Updated AState

--- src/algorithms/search/AState.java
+++ src/algorithms/search/AState.java
@@ -1,5 +1,11 @@
 package algorithms.search;
 
+import java.util.List;
+
 public abstract class AState {
+
+
+    public abstract List<AState> getSuccessors();
+    public abstract boolean equals(AState otherState);
 
 }
