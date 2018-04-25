017f1ccf55d74745f1b180cfab4a8e13998574f3
J-morag <38430800+J-morag@users.noreply.github.com>
2018-04-25 19:33:43 +0300

    ISearchingAlgo

--- src/algorithms/search/BreadthFirstSearch.java
+++ src/algorithms/search/BreadthFirstSearch.java
@@ -2,12 +2,16 @@ package algorithms.search;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BreadthFirstSearch extends ASearchingAlgorithm {
 
+    Queue<AState> greyVertices;
+
     public BreadthFirstSearch() {
         super();
+        Queue<AState> greyVertices = new LinkedList<AState>();
     }
 
     @Override
@@ -15,8 +19,14 @@ public class BreadthFirstSearch extends ASearchingAlgorithm {
         Solution solution = new Solution();
         AState startState = searchProblem.getStartState();
         AState goalState = searchProblem.getGoalState();
-        //ArrayList<AState> vertices = searchProblem.
-        //HashMap<AState> visited = new HashMap()
+        HashMap<AState, Boolean> blackVertices =
+                new HashMap<AState, Boolean>((int)(searchProblem.getAllPossibleStates().size()/0.75) +1 );
+                                                                    //(max number of entries divided by load factor) +1
+
+        // TODO insert algorithm!
+
+
+
 
 
         return null;
