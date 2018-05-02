f9e7149fe6acd28b3d36a7c94e5ebdd79019b779
shaharar <shaharar@post.bgu.ac.il>
2018-05-02 21:16:52 +0300

    after merge3

--- src/algorithms/search/ASearchingAlgorithm.java
+++ src/algorithms/search/ASearchingAlgorithm.java
@@ -23,23 +23,21 @@ public abstract class ASearchingAlgorithm implements ISearchingAlgorithm {
         Solution solution = new Solution();
         AState startState = searchProblem.getStartState();
         AState goalState = searchProblem.getGoalState();
-//      List<AState> vertices = searchProblem.getAllPossibleStates();
         Set<AState> visitedVertices = new HashSet<>((int)(vertices.size()/0.75) +1);
-//        HashMap<AState, AState> pi = new HashMap<AState, AState>((int)(vertices.size()/0.75) +1 );
         HashMap<AState, Integer> distance = new HashMap<AState, Integer>((int)(vertices.size()/0.75) +1 );
         distance.put(startState, 0);
 
         //fill solution
-        if(null != runAlgorithm(searchProblem, startState, goalState, visitedVertices, distance)){
+        if(null != runAlgorithm(searchProblem, startState, goalState, visitedVertices)){
             AState stepInSolution = goalState;
             while(null != stepInSolution){
                 solution.add(0, stepInSolution);
-                stepInSolution = pi.get(stepInSolution);
+                stepInSolution = stepInSolution.getParent();
             }
             return solution;
         }
         else return null;
     }
 
-    protected abstract AState runAlgorithm(ISearchable searchProblem, AState startState, AState goalState, Set<AState> visitedVertices, HashMap<AState, Integer> distance);
+    protected abstract AState runAlgorithm(ISearchable searchProblem, AState startState, AState goalState, Set<AState> visitedVertices);
 }
