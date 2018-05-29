6cd934adb8c99cf954df52d579b299559321a20a
shaharar <shaharar@post.bgu.ac.il>
2018-05-29 18:36:28 +0300

    server

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 
 //import Server.Strategies.IServerStrategy;
@@ -13,6 +15,7 @@ public class Server {
     private int listeningIntervalMS;
     private IServerStrategy serverStrategy;
     private volatile boolean stop;
+    //private static final Logger LOG = LogManager.getLogger();
 
     public Server(int port, int listeningIntervalMS, IServerStrategy serverStrategy) {
         this.port = port;
@@ -21,6 +24,10 @@ public class Server {
     }
 
     public void start() {
+        new Thread(() -> {runServer();}).start();
+    }
+
+    public void runServer() {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(listeningIntervalMS);
@@ -29,19 +36,23 @@ public class Server {
                 try {
                     Socket clientSocket = serverSocket.accept(); // blocking call
                     //LOG.info(String.format("Client excepted: %s",clientSocket.toString()));
-                    try {
-                        serverStrategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
-                        clientSocket.getInputStream().close();
-                        clientSocket.getOutputStream().close();
-                        clientSocket.close();
-                    } catch (IOException e) {
-                        //LOG.error("IOException", e);
-                    }
+                    new Thread(() -> {handleClient(clientSocket);}).start();
                 } catch (SocketTimeoutException e) {
                     //LOG.debug("Socket Timeout - no Client requests!");
                 }
             }
             serverSocket.close();
+        } catch (IOException e) {
+            //LOG.error("IOException", e);
+        }
+    }
+
+    private void handleClient(Socket clientSocket) {
+        try {
+            serverStrategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
+            clientSocket.getInputStream().close();
+            clientSocket.getOutputStream().close();
+            clientSocket.close();
         } catch (IOException e) {
             //LOG.error("IOException", e);
         }
